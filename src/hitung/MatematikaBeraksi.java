78f6b2f29f69185d203542c6385eebfab03a854e
Meilianda-anr <meiliandanrahman@gmail.com>
2025-06-10 21:36:58 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package hitung;
 
-/**
- *
- * @author USER
- */
+
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika meli = new Matematika(1,9);
+        
+        System.out.println("Hasuk Penjumlahan" + meli.setPenjumlahan());
+        System.out.println("Hasuk Pengurangan" + meli.setPengurangan());
+        System.out.println("Hasuk Perkalian" + meli.setPerkalian());
+        System.out.println("Hasuk Pembagian" + meli.setPembagian());
+    }
 }
